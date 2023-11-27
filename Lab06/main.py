36929b9de0478581c8c2432c8f62bf4318d71839
Kamil Krawiec <kamil.krawiec9977@gmail.com>
2023-11-27 18:54:49 +0100

    Lab06 almost finished

--- Lab06/main.py
+++ Lab06/main.py
@@ -14,9 +14,9 @@ def run_tests():
     default_batch_size = 100
 
     # Parametry do przetestowania
-    out_channels_values = [16, 32]
-    kernel_size_values = [3, 5]
-    pool_size_values = [2, 4]
+    out_channels_values = [32,126]
+    kernel_size_values = [10,15]
+    pool_size_values = [20,10]
 
     for out_channels in out_channels_values:
         for kernel_size in kernel_size_values:
@@ -32,7 +32,7 @@ def run_tests():
 
                 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
-                train_data = datasets.FashionMNIST('path', download=True, train=True, transform=transform)
+                train_data = datasets.FashionMNIST('path', download=True, train=True, transform=transform_blur)
                 test_data = datasets.FashionMNIST('path', download=True, train=False, transform=transform)
 
                 train_list = list(range(len(train_data)))
@@ -44,14 +44,15 @@ def run_tests():
                 test_20_percent = test_list[:len(test_list) // 5]
 
                 train_subset = Subset(train_data, train_20_percent)
-                train_loader = torch.utils.data.DataLoader(train_subset, batch_size=default_batch_size, shuffle=True)
+                train_loader = torch.utils.data.DataLoader(train_subset, batch_size=default_batch_size,
+                                                           shuffle=True)
 
                 test_subset = Subset(test_data, test_20_percent)
                 test_loader = torch.utils.data.DataLoader(test_subset, batch_size=default_batch_size, shuffle=True)
 
                 criterion = nn.CrossEntropyLoss()
 
-                epochs = 5  # Małe liczby epok do szybkiego przetestowania
+                epochs = 20
 
                 dummy_tensor = torch.rand(1, 1, 28, 28)
 
@@ -69,9 +70,9 @@ def run_tests():
 
                 acc, prec, f1, rec = evaluate_model(cnn_model, test_loader)
 
-                name = f"ChanN_{out_channels}_kernelSize_{kernel_size}_PoolSize_{pool_size}_BlurTest_{False}_BlurTrain_{False}"
+                name = f"ChanN_{out_channels}_kernelSize_{kernel_size}_PoolSize_{pool_size}_BlurTest_{False}_BlurTrain_{True}"
                 show_learning_curve(train_cost_history=training_losses, test_cost_history=test_losses, name=name,
-                                    acc=acc,rec=rec,prec=prec,f1=f1)
+                                    acc=acc, rec=rec, prec=prec, f1=f1)
 
 
 if __name__ == '__main__':
